b08b6e5bbcd4e25db20543c80e98edb5fb49d46d
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2023-09-26 19:47:22 -0700

    added favoritesbtn

--- Assets/JS/Games-JS/script.js
+++ Assets/JS/Games-JS/script.js
@@ -4,6 +4,7 @@ const closeModalBtn = document.getElementById("closeModalBtn");
 const modal = document.getElementById("gamesModal");
 const gamesList = document.getElementById("gamesList");
 const loadMoreBtn = document.getElementById("loadMoreBtn");
+const favoriteButtons = document.querySelectorAll(".favoriteBtn"); // favorite buttons
   
   const apiKey = "780187723c4d434eaa43adc592fca0c9"; //  API key from RAWG API
   const baseUrl = `https://api.rawg.io/api/games?key=780187723c4d434eaa43adc592fca0c9&`;
@@ -24,6 +25,7 @@ const loadMoreBtn = document.getElementById("loadMoreBtn");
                       <h3>${game.name}</h3>
                       <img src="${game.background_image}" alt="${game.name}" style="width: 400px; height: 350px;">
                       <p>Platforms: ${game.platforms.map((platform) => platform.platform.name).join(", ")}</p>
+                      <button class="favoriteBtn">Favorite</button>
                   `;
                   gamesList.appendChild(gameElement);
               });
@@ -46,5 +48,12 @@ const loadMoreBtn = document.getElementById("loadMoreBtn");
       page++;
       fetchGames();
   });
-});
 
+//   Added favorites function button
+  favoriteButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        const buttonText = event.target.innerText;
+        event.target.innerText = buttonText === "Favorite" ? "Unfavorite" : "Favorite";
+    });
+  })
+});
